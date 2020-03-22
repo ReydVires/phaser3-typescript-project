18060c9c5db939b48d0fcc07e7ee8f879f949878
ReydVires <ahmad.fire@gmail.com>
2020-03-22 11:49:05 +0700

    update font Type

--- src/scripts/components/UI/FlatButton.ts
+++ src/scripts/components/UI/FlatButton.ts
@@ -78,7 +78,7 @@ export class FlatButton extends Phaser.GameObjects.Rectangle {
 		return this._label;
 	}
 
-	public setText (text: string, style?: Phaser.Types.GameObjects.Text.TextMetrics): this {
+	public setText (text: string, style?: Phaser.Types.GameObjects.Text.TextStyle): this {
 		this._label?.setText(text);
 		if (style) {
 			this._label?.setStyle(style);
