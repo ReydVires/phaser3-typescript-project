d1e4527a0e9659c15675054f1984330e970d80be
ReydVires <ahmad.fire@gmail.com>
2020-04-13 01:26:53 +0700

    implement buttons and move fps object

--- src/scripts/scenes/TestScene.ts
+++ src/scripts/scenes/TestScene.ts
@@ -1,12 +1,10 @@
-import { PhaserLogo } from '../objects/PhaserLogo';
-import { FPSText } from '../objects/FPSText';
+import { PhaserLogo } from '../gameobjects/PhaserLogo';
 import { BaseScene } from '../components/abstract/BaseScene';
 import { centerX, centerY } from '../config';
 
 export class TestScene extends BaseScene {
 
 	private _phaserLogo: PhaserLogo;
-	private _fpsText: Phaser.GameObjects.Text;
 
 	constructor () {
 		super('TestScene');
@@ -14,16 +12,14 @@ export class TestScene extends BaseScene {
 
 	init (): void {
 		super.init();
-		console.log(`TestScene: For experimental only!`);
+		console.log(`Create TestScene`);
 	}
 
 	create (): void {
 		this._phaserLogo = new PhaserLogo(this, centerX, centerY);
-		this._fpsText = new FPSText(this);
 	}
 
 	update (): void {
-		this._fpsText.update();
 		const space = this.input.keyboard.addKey('SPACE');
 		if (Phaser.Input.Keyboard.JustDown(space)) {
 			this._phaserLogo.jump();
--- src/scripts/scenes/UITestScene.ts
+++ src/scripts/scenes/UITestScene.ts
@@ -1,6 +1,13 @@
 import { UIScene } from "../components/abstract/UIScene";
+import { FPSText } from "../gameobjects/FPSText";
+import { ToggleButton } from "../components/UI/ToggleButton";
+import { TapButton } from "../components/UI/TapButton";
+import { Button } from "../components/UI/Button";
+import { HoldButton } from "../components/UI/HoldButton";
 
 export class UITestScene extends UIScene {
+
+	private _fpsText: Phaser.GameObjects.Text;
 
 	constructor () {
 		super('UITestScene');
@@ -11,6 +18,16 @@ export class UITestScene extends UIScene {
 		console.log("Create UITestScene!");
 	}
 
-	create (): void {}
+	create (): void {
+		this._fpsText = new FPSText(this);
+		new ToggleButton(this, 180, 256, { active: 'phaser-logo', deactive: 'phaser_logo' });
+		new TapButton(this, 335, 256, 'phaser_logo');
+		new Button(this, 180, 400, 'phaser-logo', 'phaser_logo');
+		new HoldButton(this, 360, 640, 'phaser-logo');
+	}
+
+	update (): void {
+		this._fpsText.update();
+	}
 
 }
