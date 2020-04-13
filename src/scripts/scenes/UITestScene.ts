57098a62c09b65e3e01b6408d5780a7da253d171
ReydVires <ahmad.fire@gmail.com>
2020-04-14 06:09:59 +0700

    example of use joystick and communication scenes

--- src/scripts/scenes/UITestScene.ts
+++ src/scripts/scenes/UITestScene.ts
@@ -4,6 +4,7 @@ import { ToggleButton } from "../components/UI/ToggleButton";
 import { TapButton } from "../components/UI/TapButton";
 import { Button } from "../components/UI/Button";
 import { HoldButton } from "../components/UI/HoldButton";
+import { Joystick } from "../components/UI/Joystick";
 
 export class UITestScene extends UIScene {
 
@@ -20,16 +21,28 @@ export class UITestScene extends UIScene {
 
 	create (): void {
 		this._fpsText = new FPSText(this);
-		new ToggleButton(this, 180, 256, { active: 'phaser-logo', deactive: 'phaser_logo' });
-		new TapButton(this, 375, 256, 'phaser_logo')
+		new TapButton(this, 475, 256, 'phaser_logo')
 			.setText("TESTING", { fontStyle: 'bold' });
-		const btn = new Button(this, 180, 400, 'phaser-logo', 'phaser_logo')
+		const btn = new Button(this, 180, 460, 'phaser-logo', 'phaser_logo')
 			.setText("CLICK ME!", { fontStyle: 'bold' })
-			.setCallback(() => this.eventHandler.emit('UI#destroy_fps'));
+			.setCallback(() => {
+				this.eventHandler.emit('UI#destroy_fps');
+				this.eventHandler.emit('event#set_to_control');
+			});
 		btn.getLabel().setFontSize(28);
-		new HoldButton(this, 360, 640, 'phaser-logo');
+		new HoldButton(this, 360, 1040, 'phaser-logo');
+		const joystick = new Joystick(this, 360, 640, 'virtual_joystick')
+			.setToControl(this._fpsText);
+		new ToggleButton(this, 180, 256, { active: 'phaser-logo', deactive: 'phaser_logo' }, true)
+			.setCallback((value: boolean) => {
+				joystick.setActive(value);
+			});
 
 		this.registerEvent('destroy_fps', () => this._fpsText.destroy(), true);
+		this.registerEvent('set_to_control', (target: unknown) => {
+			const gameObject = Array.isArray(target) ? target.pop() : null;
+			joystick.setToControl(gameObject);
+		}, true);
 	}
 
 	update (): void {
