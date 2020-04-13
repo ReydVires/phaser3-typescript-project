eb70064ff1451f7cbdbc3e490a98097234324574
ReydVires <ahmad.fire@gmail.com>
2020-04-13 16:39:59 +0700

    implements button to UIScene

--- src/scripts/scenes/UITestScene.ts
+++ src/scripts/scenes/UITestScene.ts
@@ -21,9 +21,12 @@ export class UITestScene extends UIScene {
 	create (): void {
 		this._fpsText = new FPSText(this);
 		new ToggleButton(this, 180, 256, { active: 'phaser-logo', deactive: 'phaser_logo' });
-		new TapButton(this, 335, 256, 'phaser_logo');
-		new Button(this, 180, 400, 'phaser-logo', 'phaser_logo')
+		new TapButton(this, 375, 256, 'phaser_logo')
+			.setText("TESTING", { fontStyle: 'bold' });
+		const btn = new Button(this, 180, 400, 'phaser-logo', 'phaser_logo')
+			.setText("CLICK ME!", { fontStyle: 'bold' })
 			.setCallback(() => this.eventHandler.emit('UI#destroy_fps'));
+		btn.label.setFontSize(28);
 		new HoldButton(this, 360, 640, 'phaser-logo');
 
 		this.registerEvent('destroy_fps', () => this._fpsText.destroy(), true);
